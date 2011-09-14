9a8cac8201f3c8eaeafa90bc9088f525bb4b61b8
Gareth Estchild <gareth.estchild@gmail.com>
2011-09-14 20:31:45 +0100

    re-writing comments

--- jquery.newwindow.js
+++ jquery.newwindow.js
@@ -1,21 +1,19 @@
-(function($) {
-    
+(function($) { 
     $.fn.newWindow = function() {
-	//do ones for pdf/word/ docs options {to extend files}
-//New Window:
-//Open external links in a new window
-//adds title to inform users on hover that link opens in a new window;
-//adds additional text for screen readers to say it opens in a new window, screen readers don't normally read title on links; 
-//must use css to show a new window icon for sighted users and to hide assistive text off the page
-//used background image as bug in JAWS 11 stops it from reading alt text in link containing other text
         return this.each(function() {
-            $(this).addClass('newwindow-active').attr('target', '_blank');			
+			// add a class to the link that can be used for styling purposes; open the link in a new window
+            $(this).addClass('newwindow-active').attr('target', '_blank');	
+			// add/amend title to inform users on hover that link opens in a new window;
 			var title = $(this).attr('title');
 			title
 			? $(this).attr('title', title + ' (Opens in a new window)')
 			: $(this).attr('title', 'This link opens in a new window');
+			// add additional text for screen reader/keyboard only users to inform them that the link opens in a new window
+			// screen readers don't normally read title attributes on links; keyboard only users won't activate the tooltip
 			$('<span class="assistive-text"> (opens in a new window)</span>').appendTo($(this));  
+			// if desired use CSS image replacement technique to show a new window icon for sighted users and hide the assistive text off the page
         });
     };
-    
 })(jQuery);
+
+
